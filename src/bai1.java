c940103f00c2ea8985feebcafcb889ed77659536
Hoaivy <levuhoaivy@gmail.com>
2026-04-29 20:44:26 +0700

    Nop bai tap Java

---
+++ src/bai1.java
@@ -0,0 +1,21 @@
+public class bai1 {
+    public static void main(String[] args) {
+        String s = "Cybersoft";
+        int len = s.length();
+        System.out.println("Chuỗi: " + s);
+
+        String result = "";
+
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+            } else {
+                result = result + c;
+            }
+        }
+
+        System.out.println("Chuỗi sau khi xóa nguyên âm: " + result);
+    }
+}
