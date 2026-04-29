5cfdf0971a19788cf223ce85da49f40deba2ec97
Hoaivy <levuhoaivy@gmail.com>
2026-04-30 00:08:20 +0700

    hoan thanh bai 1

--- src/bai1.java
+++ src/bai1.java
@@ -9,10 +9,10 @@ public class bai1 {
         for (int i = 0; i < len; i++) {
             char c = s.charAt(i);
 
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
-            } else {
-                result = result + c;
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
+                    c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U') {
+
+                result = result + c; // Thì mới cộng vào kết quả
             }
         }
 
